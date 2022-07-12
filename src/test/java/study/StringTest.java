f0ebb514a78c9cdfa6d2a37d09f76364a20bc756
Hong <gtv7854@naver.com>
2022-07-12 13:54:50 +0900

    Calculator impl / StringTest Trainig

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,28 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        /*Require 1-1*/
+        String[] Require = "1,2".split(",");
+        assertThat(Require).contains("1","2");
+
+        /*Require 1-2*/
+        String[] Require1 = "1".split(",");
+        assertThat(Require1).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        /*Require 2*/
+        String Require2 = "(1,2)".substring(1,"(1,2)".length()-1);
+        assertThat(Require2).isEqualTo("1,2");
+    }
+
+    @Test
+    void charat(){
+        /*Require 3*/
+    }
 }
+
